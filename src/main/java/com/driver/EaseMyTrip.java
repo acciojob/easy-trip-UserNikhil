c5ac406ff41734832d6a721b3cafd0fa9b5f1743
Vinod Kumar <neelavinod26@gmail.com>
2023-05-27 20:34:46 +0530

    Done

--- src/main/java/com/driver/EaseMyTrip.java
+++ src/main/java/com/driver/EaseMyTrip.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EaseMyTrip {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EaseMyTrip.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EaseMyTrip.class, args);
+    }
 
 }
